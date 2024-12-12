39efe55999d7e54962e833af3c7a2859613be24d
Charlie Doherty <charlieedoherty@gmail.com>
2024-12-12 15:05:56 -0600

    Improve fractional arithmetic

--- calculator.cpp
+++ calculator.cpp
@@ -6,82 +6,96 @@
 using namespace std;
 
 // defines order of operations
-int precedence(char op) {
-    switch (op) {
-        case '+':
-        case '-':
-            return 1;
-        case '*':
-        case '/':
-        case '%':
-            return 2;
-        case '^':
-            return 3;
+int precedence(char op)
+{
+    switch (op)
+    {
+    case '+':
+    case '-':
+        return 1;
+    case '*':
+    case '/':
+    case '%':
+        return 2;
+    case '^':
+        return 3;
     }
     return -1;
 }
 
-int operation(int a, int b, char op){
+float operation(float a, float b, char op)
+{
     // returns the output of the desired operation
-    switch (op) {
-        case '+':
-            return a + b;
-        case '-':
-            return a - b;
-        case '*':
-            return a * b;
-        case '/':
-            if (b == 0) {
-                // does not allow divide by zero
-                throw runtime_error("Division by zero");
-            }
-            return a / b;
-        case '%':
-            if (b == 0) {
-                // does not allow modulo by zero
-                throw runtime_error("Modulo by zero");
-            }
-            return a % b;
-        case '^':
-            return pow(a, b);
+    switch (op)
+    {
+    case '+':
+        return a + b;
+    case '-':
+        return a - b;
+    case '*':
+        return a * b;
+    case '/':
+        if (b == 0)
+        {
+            // does not allow divide by zero
+            throw runtime_error("Division by zero");
+        }
+        return a / b;
+    case '%':
+        if (b == 0)
+        {
+            // does not allow modulo by zero
+            throw runtime_error("Modulo by zero");
+        }
+        return (int)(a) % (int)(b);
+    case '^':
+        return pow(a, b);
     }
     // or zero if incorrect operator
     return 0;
 }
 
-int evaluate(string expression){
-    stack<int> values;
+float evaluate(string expression)
+{
+    stack<float> values;
     stack<char> operators;
-    int i = 0;  // Start index for parsing
+    int i = 0; // Start index for parsing
     // Evaluates the expression
-    while (i < expression.length()) {
-        if (expression[i] == ' ') {
+    while (i < expression.length())
+    {
+        if (expression[i] == ' ')
+        {
             // skips the spaces in the expression
             i++;
             continue;
         }
-        
+
         // Evaluates Digits
-        if (isdigit(expression[i])) {
-            int num = 0;
-            while (i < expression.length() && isdigit(expression[i])) {
-                num = num * 10 + (int)(expression[i] - '0');
+        if (isdigit(expression[i]))
+        {
+            float num = 0;
+            while (i < expression.length() && isdigit(expression[i]))
+            {
+                num = num * 10 + (float)(expression[i] - '0');
                 i++;
             }
             values.push(num);
-        } 
+        }
         // Evaluates Parenthesis
-        else if (expression[i] == '(') {
+        else if (expression[i] == '(')
+        {
             operators.push(expression[i]);
             i++;
         }
-        else if (expression[i] == ')') {
-            while (!operators.empty() && operators.top() != '(') {
+        else if (expression[i] == ')')
+        {
+            while (!operators.empty() && operators.top() != '(')
+            {
                 // If closes out a left parenthesis with operators/digits inside of it:
                 // Solves equation
-                int val2 = values.top();
+                float val2 = values.top();
                 values.pop();
-                int val1 = values.top();
+                float val1 = values.top();
                 values.pop();
                 char op = operators.top();
                 operators.pop();
@@ -90,13 +104,15 @@ int evaluate(string expression){
             operators.pop();
             i++;
         }
-        else {
+        else
+        {
             // Evaluates high precedence operators first (%,/,x)
-            while (!operators.empty() && precedence(expression[i]) <= precedence(operators.top())) {
+            while (!operators.empty() && precedence(expression[i]) <= precedence(operators.top()))
+            {
                 // Solves equation
-                int val2 = values.top();
+                float val2 = values.top();
                 values.pop();
-                int val1 = values.top();
+                float val1 = values.top();
                 values.pop();
                 char op = operators.top();
                 operators.pop();
@@ -108,10 +124,11 @@ int evaluate(string expression){
     }
 
     // Evaluates the rest of the operators
-    while (!operators.empty()) {
-        int val2 = values.top();
+    while (!operators.empty())
+    {
+        float val2 = values.top();
         values.pop();
-        int val1 = values.top();
+        float val1 = values.top();
         values.pop();
         char op = operators.top();
         operators.pop();
@@ -121,17 +138,24 @@ int evaluate(string expression){
     return values.top();
 }
 
-// Input handling  
-int main() {
+// Input handling
+int main()
+{
     string expr;
-    cout << "Enter an arithmetic expression: ";
-    getline(cin, expr);
+    while (true)
+    {
+        cout << "Enter an arithmetic expression: ";
+        getline(cin, expr);
 
-    try {
-        cout << "Result: " << evaluate(expr) << endl;
-    } catch (const runtime_error& e) {
-        // error handling
-        cerr << "Error: " << e.what() << endl;
+        try
+        {
+            cout << "Result: " << evaluate(expr) << endl;
+        }
+        catch (const runtime_error &e)
+        {
+            // error handling
+            cerr << "Error: " << e.what() << endl;
+        }
     }
 
     return 0;
